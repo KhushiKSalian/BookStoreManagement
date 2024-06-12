8187338f937618e1dae7e8ea1479b6b4d0eddabe
KhushiKSalian <khushiksalian@gmail.com>
2024-06-12 09:23:38 +0530

    Navbar

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const links=[
@@ -27,22 +28,24 @@ const Navbar = () => {
 
   return (
   <div className='flex bg-zinc-800 text-white px-8 py-4 item-center justify-between'>
-    <div className='flex items-center'>
+    <Link to="/" className='flex items-center'>
         <img 
         className='h-10 me-4'
         src='https://cdn-icons-png.flaticon.com/128/5833/5833290.png' 
         alt='logo'/>
         <h1 className='text-2xl font-semibold'>Bookstore</h1>
-    </div>
+    </Link>
     <div className='nav-links-bookheaven flex items-center gap-4'>
        <div className=' flex gap-4'> 
         {links.map((items,i)=>(
-            <div className='hover:text-blue-500 transition-all duration-300'
-        key={i}>{items.title}{" "}</div>))}
+            <Link to={items.link}
+            className='hover:text-blue-500 transition-all duration-300'
+        key={i}>{items.title}{" "}
+        </Link>))}
        </div>
        <div className='flex gap-4'>
-            <button className='px-2 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300 '>Log in</button>
-            <button className='px-2 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300 '>Sign up</button>
+            <Link to="/LogIn" className='px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300 '>Log in</Link>
+            <Link to="/SignUp" className='px-4 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300 '>Sign up</Link>
        </div>
      </div>
   </div>
